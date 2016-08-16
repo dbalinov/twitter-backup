1f0be709e03d9c4d9963ffcf13311d1580b16825
dbalinov <dnbalinov@gmail.com>
2016-08-16 12:27:20 +0300

    Add initial web project.

---
+++ TwitterBackup/TwitterBackup.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwitterBackup.Web.Startup))]
+namespace TwitterBackup.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
