61bb4872ff98c3428883493f9617daebf27c458d
dbalinov <dnbalinov@gmail.com>
2016-08-16 13:38:13 +0300

    Clean up unnecessary code.

--- TwitterBackup/TwitterBackup.Web/Controllers/HomeController.cs
+++ TwitterBackup/TwitterBackup.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace TwitterBackup.Web.Controllers
 {
@@ -16,13 +12,6 @@ namespace TwitterBackup.Web.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
