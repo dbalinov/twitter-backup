b42374ac1ddcd6b1b633642156110403b32a3009
dbalinov <dnbalinov@gmail.com>
2016-08-25 23:33:17 +0300

    Add StatusStoreControllerTests

--- TwitterBackup/Test/TwitterBackup.Web.Tests/Controllers/StatusStoreControllerTests.cs
+++ TwitterBackup/Test/TwitterBackup.Web.Tests/Controllers/StatusStoreControllerTests.cs
@@ -1,6 +1,10 @@
-﻿using TwitterBackup.Business.Services.Statuses;
+﻿using System.Threading.Tasks;
+using System.Web.Http.Results;
 using NSubstitute;
+using TwitterBackup.Business.Services.Statuses;
 using TwitterBackup.Web.Controllers;
+using TwitterBackup.Web.Models.Status;
+using Xunit;
 
 namespace TwitterBackup.Web.Tests.Controllers
 {
@@ -15,6 +19,36 @@ namespace TwitterBackup.Web.Tests.Controllers
             this.statusService = Substitute.For<IStatusService>();
 
             this.statusStoreController = new StatusStoreController(this.statusService);
+        }
+
+        [Fact]
+        public async Task PostTest()
+        {
+            // Arrange 
+            var request = new StatusIdRequest { StatusId = "id" };
+
+            // Act
+            var resultRaw = await this.statusStoreController.Post(request);
+            var result = resultRaw as OkResult;
+
+            // Assert
+            Assert.NotNull(result);
+            await this.statusService.Received().SaveAsync(request.StatusId);
+        }
+
+        [Fact]
+        public async Task DeleteTest()
+        {
+            // Arrange 
+            var request = new StatusIdRequest { StatusId = "id" };
+
+            // Act
+            var resultRaw = await this.statusStoreController.Delete(request);
+            var result = resultRaw as OkResult;
+
+            // Assert
+            Assert.NotNull(result);
+            await this.statusService.Received().UnsaveAsync(request.StatusId);
         }
     }
 }
