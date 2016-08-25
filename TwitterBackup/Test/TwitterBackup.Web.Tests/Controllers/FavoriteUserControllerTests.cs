9f260feb2816ee7cb3106195a69453d66b6e5adb
dbalinov <dnbalinov@gmail.com>
2016-08-25 23:57:57 +0300

    FavoriteUserControllerTests

--- TwitterBackup/Test/TwitterBackup.Web.Tests/Controllers/FavoriteUserControllerTests.cs
+++ TwitterBackup/Test/TwitterBackup.Web.Tests/Controllers/FavoriteUserControllerTests.cs
@@ -6,8 +6,8 @@ using TwitterBackup.Business.Models;
 using TwitterBackup.Business.Services.Users;
 using TwitterBackup.Infrastructure.Identity.Claims;
 using TwitterBackup.Web.Controllers;
-using TwitterBackup.Web.Models.User;
 using Xunit;
+using TwitterBackup.Web.Models.User;
 
 namespace TwitterBackup.Web.Tests.Controllers
 {
@@ -16,51 +16,68 @@ namespace TwitterBackup.Web.Tests.Controllers
         private readonly IFavoriteUserService favoriteUserService;
         private readonly ITwitterClaimsHelper claimsHelper;
 
-        private readonly FavoriteUserController dashboardController;
+        private readonly FavoriteUserController favoriteUserController;
   
-
         public FavoriteUserControllerTests()
         {
             this.favoriteUserService = Substitute.For<IFavoriteUserService>();
             this.claimsHelper = Substitute.For<ITwitterClaimsHelper>();
 
-            this.dashboardController = new FavoriteUserController(this.favoriteUserService, this.claimsHelper);
+            this.favoriteUserController = new FavoriteUserController(this.favoriteUserService, this.claimsHelper);
         }
 
+        [Fact]
+        public async Task GetFavoriteUsersTest()
+        {
+            // Arrange
+            var userId = "user id";
+            var users = new List<UserModel>();
 
-        //public async Task<IHttpActionResult> GetFavoriteUsers()
-        //{
-        //    var userId = claimsHelper.GetUserId();
-        //    var users = await this.favoriteUserService.GetFavoriteUsers(userId);
+            claimsHelper.GetUserId().Returns(userId);
+            this.favoriteUserService.GetFavoriteUsers(userId).Returns(users);
 
-        //    return Ok(users);
-        //}
+            // Act
+            var resultRaw = await this.favoriteUserController.GetFavoriteUsers();
+            var result = resultRaw as OkNegotiatedContentResult<IEnumerable<UserModel>>;
 
-        //public async Task<IHttpActionResult> Put(FavoriteUserRequest request)
-        //{
-        //    var relation = GetRelation(request);
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Content);
+            Assert.Equal(users, result.Content);
+        }
 
-        //    await this.favoriteUserService.AddAsync(relation);
+        [Fact]
+        public async Task PutTest()
+        {
+            // Arrange
+            var request = new FavoriteUserRequest();
 
-        //    return Ok();
-        //}
+            // Act
+            var resultRaw = await this.favoriteUserController.Put(request);
+            var result = resultRaw as OkResult;
 
-        //public async Task<IHttpActionResult> Delete([FromUri]FavoriteUserRequest request)
-        //{
-        //    var relation = GetRelation(request);
+            // Assert
+            Assert.NotNull(result);
 
-        //    await this.favoriteUserService.RemoveAsync(relation);
+            await this.favoriteUserService.Received()
+                .AddAsync(Arg.Any<FavoriteUserRelationModel>());
+        }
 
-        //    return Ok();
-        //}
+        [Fact]
+        public async Task DeleteTest()
+        {
+            // Arrange
+            var request = new FavoriteUserRequest();
 
-        //private FavoriteUserRelationModel GetRelation(FavoriteUserRequest request)
-        //{
-        //    return new FavoriteUserRelationModel
-        //    {
-        //        SourceUserId = claimsHelper.GetUserId(),
-        //        TargetUserId = request.UserId
-        //    };
-        //}
+            // Act
+            var resultRaw = await this.favoriteUserController.Delete(request);
+            var result = resultRaw as OkResult;
+
+            // Assert
+            Assert.NotNull(result);
+
+            await this.favoriteUserService.Received()
+                .RemoveAsync(Arg.Any<FavoriteUserRelationModel>());
+        }
     }
 }
