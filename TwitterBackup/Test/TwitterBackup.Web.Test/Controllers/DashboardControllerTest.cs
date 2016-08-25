15415d450b569c680918642bb0cfc1f14cf83d47
dbalinov <dnbalinov@gmail.com>
2016-08-25 12:20:04 +0300

    Dashboard controller test.

--- TwitterBackup/Test/TwitterBackup.Web.Test/Controllers/DashboardControllerTest.cs
+++ TwitterBackup/Test/TwitterBackup.Web.Test/Controllers/DashboardControllerTest.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Web.Http.Results;
+using Business.Models;
+using Business.Services.Users;
+using NSubstitute;
+using TwitterBackup.Web.Controllers;
+using TwitterBackup.Web.Messages.User;
+using Xunit;
 
 namespace TwitterBackup.Web.Test.Controllers
 {
-    class DashboardControllerTest
+    public class DashboardControllerTest
     {
+        private readonly IUserService userService;
+        private readonly DashboardController dashboardController;
+
+        public DashboardControllerTest()
+        {
+            this.userService = Substitute.For<IUserService>();
+            this.dashboardController = new DashboardController(this.userService);
+        }
+
+        [Fact]
+        public async Task GetDataTest()
+        {
+            // Arrange 
+            var users = new List<DashboardUserModel>();
+            this.userService.GetDashboardUsersAsync().Returns(users);
+            
+            // Act
+            var resultRaw = await this.dashboardController.GetData();
+            var result = resultRaw as OkNegotiatedContentResult<DashboardResponse>;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Content);
+            Assert.NotNull(result.Content.Users);
+            Assert.Equal(result.Content.Users, users);
+        }
     }
 }
