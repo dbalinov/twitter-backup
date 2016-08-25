23e6e6966b250ed670a2aa1c611d4dc69ef4dd8f
dbalinov <dnbalinov@gmail.com>
2016-08-26 00:11:50 +0300

    Add constructores for Service tests.

--- TwitterBackup/Test/TwitterBackup.Business.Tests/Services/Users/FavoriteUserServiceTests.cs
+++ TwitterBackup/Test/TwitterBackup.Business.Tests/Services/Users/FavoriteUserServiceTests.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NSubstitute;
+using TwitterBackup.Business.Services.Users;
+using TwitterBackup.DataAccess.Repositories.Users;
 
 namespace TwitterBackup.Business.Tests.Services.Users
 {
     public class FavoriteUserServiceTests
     {
+        private readonly IFavoriteUserRepository favoriteUserRepository;
+        private readonly IUserRepository userRepository;
+
+        private readonly IFavoriteUserService favoriteUserService;
+
+        public FavoriteUserServiceTests()
+        {
+            this.favoriteUserRepository = Substitute.For<IFavoriteUserRepository>();
+            this.userRepository = Substitute.For<IUserRepository>();
+
+            this.favoriteUserService = new FavoriteUserService(this.favoriteUserRepository, this.userRepository);
+        }
     }
 }
--- TwitterBackup/Test/TwitterBackup.Business.Tests/Services/Users/UserServiceTests.cs
+++ TwitterBackup/Test/TwitterBackup.Business.Tests/Services/Users/UserServiceTests.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NSubstitute;
+using TwitterBackup.DataAccess.Repositories.Users;
+using TwitterBackup.DataAccess.Repositories.Statuses;
+using TwitterBackup.Business.Services.Users;
+using TwitterBackup.Infrastructure.Identity.Claims;
 
 namespace TwitterBackup.Business.Tests.Services.Users
 {
     public class UserServiceTests
     {
+        private readonly IUserRepository userRepository;
+        private readonly IFavoriteUserRepository favoriteUserRepository;
+        private readonly IStatusStoreRepository statusStoreRepository;
+        private readonly IStatusRepository statusRepository;
+        private readonly ITwitterClaimsHelper claimsHelper;
+
+        private readonly IUserService userService;
+
+        public UserServiceTests()
+        {
+            this.userRepository = Substitute.For<IUserRepository>();
+            this.favoriteUserRepository = Substitute.For<IFavoriteUserRepository>();
+            this.statusStoreRepository = Substitute.For<IStatusStoreRepository>();
+            this.statusRepository = Substitute.For<IStatusRepository>();
+            this.claimsHelper = Substitute.For<ITwitterClaimsHelper>();
+
+            this.userService = new UserService(
+                userRepository,
+                favoriteUserRepository,
+                statusStoreRepository,
+                statusRepository,
+                claimsHelper);
+        }
+
     }
 }
